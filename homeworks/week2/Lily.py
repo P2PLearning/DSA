5f40af1893960ce22739fa04c8207bb5fc179e22
Hien To <3415436+ubriela@users.noreply.github.com>
2020-06-27 14:34:55 -0700

    Update Lily.py

--- homeworks/week2/Lily.py
+++ homeworks/week2/Lily.py
@@ -1,3 +1,8 @@
+# Overall comments: If this is an answer during a coding interview; then No Hire (or may be borderline depending the position).
+# Details comments:
+# 1. Please provide space and time complexity.
+# 2. Please add more tests
+# 3. Please use more readable variables (uniq, ans). Acronym is usually not recommended in code.
 def removeDuplicateCharacters(s):
     uniq = set()
     ans = []
